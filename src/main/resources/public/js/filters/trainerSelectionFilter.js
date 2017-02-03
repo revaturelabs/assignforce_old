271a733ab466751dc6ed1fdd5a1f8fea8d848c0d
Nick Edwards <nickeeromo@aol.com>
2017-02-03 03:44:31 -0500

    filter maintainability fix

--- src/main/resources/public/js/filters/trainerSelectionFilter.js
+++ src/main/resources/public/js/filters/trainerSelectionFilter.js
@@ -11,20 +11,24 @@ assignforce.filter("trainerSelectionFilter", function() {
 		var filtered = [];
 
 		var count = 0;
-		//Iterates through trainer unavailable dates.
+		//Iterates through trainer unavailable date ranges.
 		for (unavailable in trainer.unavailable) {
 			unavailable[0] = new Date(unavailable[0].getYear(), unavailable[0].getMonth(), unavailable[0].getDay(), 0, 0, 0, 0);
 			unavailable[1] = new Date(unavailable[1].getYear(), unavailable[1].getMonth(), unavailable[1].getDay(), 0, 0, 0, 0);
 
+			//Iterates current unavailable date range by day
 			for (var i = trainer.unavailable[0]; i <= trainer.unavailable[1]; i.setDate(i.getDate() + 1)) {
-				for (var j = batchStart; j <= batchEnd.getDate() + 14; j.setDate()) { //14 can be replaced by a config value.
+				//Iterates batch dates by day
+				for (var j = batchStart; j <= batchEnd.getDate() + 14 && count < 10; j.setDate()) { //14 can be replaced by a config value.
 					if (i == j) {
-						count++;
-						if (count == 10)
-							break;
+						count++;						
 					}
-					filtered.push(item);
 				}
+				if (count == 10){ //If overlapped dates are 10 or greater, trainer is unavailable for this batch.
+					count = 0;
+					break;
+				}
+				filtered.push(item);
 			}
 
 			return filtered;
