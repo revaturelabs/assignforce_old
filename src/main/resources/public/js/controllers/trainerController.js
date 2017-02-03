c96bd20ee6e9ef266c44e7173b484dd4e4e5c1f2
Lazaro Perez <lazaroperez0@gmail.com>
2017-02-03 09:14:31 -0500

    removing console logs

--- src/main/resources/public/js/controllers/trainerController.js
+++ src/main/resources/public/js/controllers/trainerController.js
@@ -2,7 +2,6 @@
     var assignforce = angular.module( "batchApp" );
 
     assignforce.controller( "trainerCtrl", function( $scope, $mdDialog, trainerService ) {
-        console.log("Beginning trainer controller.");
         var tc = this;
         tc.weeks = 5;
 
@@ -46,30 +45,23 @@
           // page initialization
             // gets all trainers and stores them in variable trainers
         trainerService.getAll( function(response) {
-            // console.log("  (TC)  Retrieving all trainers.");
             tc.trainers = response;
             console.log(tc.trainers);
         }, function(error) {
-            //console.log("  (TC)  Failed to retrieve all trainers with error", error.data.message);
             tc.showToast("Could not fetch trainers.");
         });
 
         tc.rePullTrainers = function(){
             tc.trainers = undefined;
             trainerService.getAll( function(response) {
-                // console.log("  (TC)  Retrieving all trainers.");
                 tc.trainers = response;
-                // console.log(tc.trainers);
             }, function(error) {
-                //console.log("  (TC)  Failed to retrieve all trainers with error", error.data.message);
                 tc.showToast("Could not fetch trainers.");
             });
         };
 
         trainerService.getById(1, function (response) {
-            // console.log("getting a single Trainer");
             tc.singleTrainer = response;
-            // console.log(tc.namedTrainer);
         }, function (error) {
             tc.showToast("could not fetch a trainer")
         });
--- src/main/resources/public/js/controllers/trainerDialogController.js
+++ src/main/resources/public/js/controllers/trainerDialogController.js
@@ -14,27 +14,19 @@ assignforce.controller("trainerDialogCtrl", function ($scope, $mdDialog, trainer
 
     //save new
     tdc.save = function (isValid) {
-        console.log(isValid);
         if (isValid){
             if (tdc.state == "create"){
-                console.log(tdc.trainer)
                 trainerService.create(tdc.trainer, function () {
-                    console.log("pass");
                     $mdDialog.hide();
                 }, function () {
-                    console.log("cancel");
                     $mdDialog.cancel();
                 });
-
-                console.log("not called");
                 $mdDialog.hide();
             }
         }
     };
 
-
     //data
     tdc.stateMux = { "create" : "Create new trainer" };
-
 
 });
