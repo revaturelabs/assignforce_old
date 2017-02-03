0121ce17eeb15fbb73d93362ae90e283a4280875
Nick Edwards <nickeeromo@aol.com>
2017-02-03 03:56:22 -0500

    filter fix fix fix

--- src/main/resources/public/js/filters/trainerSelectionFilter.js
+++ src/main/resources/public/js/filters/trainerSelectionFilter.js
@@ -14,24 +14,25 @@ assignforce.filter("trainerSelectionFilter", function() {
 
 		//Iterates through trainer unavailable dates.
 		for (var unavailable in trainer.unavailable) {
-			count = 0;
-			unavailable[0] = new Date(unavailable[0].getYear(), unavailable[0].getMonth(), unavailable[0].getDay(), 0, 0, 0, 0);
-			unavailable[1] = new Date(unavailable[1].getYear(), unavailable[1].getMonth(), unavailable[1].getDay(), 0, 0, 0, 0);
+			if (trainer.hasOwnProperty(unavailable)) {
+				count = 0;
+				unavailable[0] = new Date(unavailable[0].getYear(), unavailable[0].getMonth(), unavailable[0].getDay(), 0, 0, 0, 0);
+				unavailable[1] = new Date(unavailable[1].getYear(), unavailable[1].getMonth(), unavailable[1].getDay(), 0, 0, 0, 0);
 
-			//Iterates current unavailable date range by day
-			for (var i = trainer.unavailable[0]; i <= trainer.unavailable[1]; i.setDate(i.getDate() + 1)) {
-				//Iterates batch dates by day
-				for (var j = batchStart; j <= batchEnd.getDate() + 14 && count < 10; j.setDate()) { //14 can be replaced by a config value.
-					if (i == j)
-						count = count + 1;
+				//Iterates current unavailable date range by day
+				for (var i = trainer.unavailable[0]; i <= trainer.unavailable[1]; i.setDate(i.getDate() + 1)) {
+					//Iterates batch dates by day
+					for (var j = batchStart; j <= batchEnd.getDate() + 14 && count < 10; j.setDate()) { //14 can be replaced by a config value.
+						if (i == j)
+							count = count + 1;
+					}
 				}
+				if (count == 10) { //If overlapped dates are 10 or greater, trainer is unavailable for this batch.
+					break;
+				}
+				filtered.push(item);
 			}
-			if (count == 10) { //If overlapped dates are 10 or greater, trainer is unavailable for this batch.
-				break;
-			}
-			filtered.push(item);
 		}
 		return filtered;
-
 	}
 });
