8dbaeb3cb94c02162ab71d8199090576e185d615
Nate Vardell <nvardell@gmail.com>
2017-02-02 21:52:28 -0500

    SonarQube killed it - Reverted back to normal

--- src/main/java/com/revature/assignforce/AssignForceV2Application.java
+++ src/main/java/com/revature/assignforce/AssignForceV2Application.java
@@ -12,10 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages={"com.revature.assignforce.domain.dao"})
 @EntityScan("com.revature.assignforce.domain")
 public class AssignForceV2Application {
-
-	private AssignForceV2Application() {
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AssignForceV2Application.class, args);
 	}
