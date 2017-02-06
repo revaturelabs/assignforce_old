9c410088b18e029822a1b43d7bd558a3bac1b551
Nate Vardell <nvardell@gmail.com>
2017-02-05 23:33:05 -0500

    Updates for Reports Tab

--- src/main/resources/public/js/controllers/reportController.js
+++ src/main/resources/public/js/controllers/reportController.js
@@ -1,20 +1,42 @@
+// ReportsController.js
 
 var assignforce = angular.module( "batchApp" );
 
-assignforce.controller( "reportCtrl", function( $scope, batchService, curriculumService, monthList ){
-
+assignforce.controller( "reportCtrl", function( $scope, batchService, curriculumService, monthList ) {
+    //console.log("Beginning report controller.");
     var rc = this;
+
+
+
+
+    $scope.options = [{
+        title: 'Item1',
+        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+    }, {
+        title: 'Item2',
+        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+    }, {
+        title: 'Item3',
+        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+    }, {
+        title: 'Item4',
+        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+    }];
+
+
+
+
 
     // functions
     // calls showToast method of aCtrl
-    rc.showToast = function( message ) {
-        $scope.$parent.aCtrl.showToast( message );
+    rc.showToast = function (message) {
+        $scope.$parent.aCtrl.showToast(message);
     };
 
     // formats data to be exported as .csv file
-    rc.export = function() {
+    rc.export = function () {
         var formatted = [];
-        formatted.push( [
+        formatted.push([
             "Curriculum",
             "January",
             "February",
@@ -30,17 +52,17 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
             "December",
             "Total"
         ]);
-        rc.curricula.forEach( function(curr) {
+        rc.curricula.forEach(function (curr) {
             var year = [curr.name];
             var sum = 0;
-            rc.currSummary(curr).forEach( function(month) {
+            rc.currSummary(curr).forEach(function (month) {
                 year.push(month);
                 sum += month;
             });
             year.push(sum);
 
             formatted.push(year);
-        })
+        });
 
         var totalMonth = ["Total"];
         var sumTotal = 0;
@@ -57,39 +79,39 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
     };
 
     // summarizes graduate output of given curriculum for chosen year
-    rc.currSummary = function( curriculum ){
+    rc.currSummary = function (curriculum) {
 
         var summary = [];
-        var total;
+        var total = 0;
 
         for (var month = 0; month < 12; month++) {
             total = 0;
-            for (var batch in rc.branches){
-                if(rc.branches.hasOwnProperty(batch) && (batch.curriculum && curriculum)){
+            rc.batches.forEach(function (batch) {
+
+                if (batch.curriculum && curriculum) {
                     date = new Date(batch.endDate);
-                    if ( (date.getMonth() == month) && (date.getFullYear() == rc.year) && (batch.curriculum.id == curriculum.id) ) {
+                    if ((date.getMonth() == month) && (date.getFullYear() == rc.year) && (batch.curriculum.id == curriculum.id)) {
                         total += rc.graduates;
                     }
                 }
-            }
-
-            summary.push( total );
+            });
+            summary.push(total);
         }
 
         return summary;
     };
 
     // sums months for given curriculum in chosen year
-    rc.sumCurrYear = function( total, num ){
+    rc.sumCurrYear = function (total, num) {
         return total + num;
     };
 
     // sums all curricula for the year
-    rc.sumYear = function() {
+    rc.sumYear = function () {
 
         var total = 0;
         var summary;
-        rc.curricula.forEach( function(curr){
+        rc.curricula.forEach(function (curr) {
             summary = rc.currSummary(curr);
             total += summary.reduce(rc.sumCurrYear);
         });
@@ -97,14 +119,14 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
     };
 
     // sums monthly total over all curricula
-    rc.sumMonth = function(month){
+    rc.sumMonth = function (month) {
 
         if (rc.batches) {
             var total = 0;
             var date;
-            rc.batches.forEach( function(batch){
+            rc.batches.forEach(function (batch) {
                 date = new Date(batch.endDate);
-                if ( (date.getMonth() == month) && (date.getFullYear() == rc.year) && (batch.curriculum) ) {
+                if ((date.getMonth() == month) && (date.getFullYear() == rc.year) && (batch.curriculum)) {
                     total += rc.graduates;
                 }
             });
@@ -115,6 +137,7 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
     // data
     rc.year = new Date().getFullYear();
     rc.graduates = 15;
+    rc.futureGrads = 15;
 
     rc.currOrder = "name";
 
@@ -122,24 +145,113 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
 
     // page initialization
     // data gathering
-    batchService.getAll( function(response) {
+    batchService.getAll(function (response) {
         //console.log("  (RC)  Retrieving all batches.");
         rc.batches = response;
-    }, function(error) {
+    }, function (error) {
         //console.log("  (RC)  Failed to retrieve all batches with error:", error.data.message);
-        rc.showToast( "Could not fetch batches.");
+        rc.showToast("Could not fetch batches.");
     });
 
-    curriculumService.getAll( function(response) {
-        //console.log("  (RC)  Retrieving all curricula.");
+    curriculumService.getAll(function (response) {
+        console.log("  (RC)  Retrieving all curricula.");
         rc.curricula = response;
-    }, function(error) {
+        console.log(response);
+    }, function (error) {
         //console.log("  (RC)  Failed to retrieve all curricula with error:", error.data.message);
-        rc.showToast( "Could not fetch curricula.");
+        rc.showToast("Could not fetch curricula.");
     });
 
-    // only batches and curricula are necessary now, but these are here in the event that new reports require the use of other object lists
+    // Only batches and curricula are necessary now, but these are here in the event that new reports require the use of other object lists
     // Nate Vardell Deleted the commented out block that was here because SonarQube wouldn't pass with it.
     // Contained 4 functions for getting skills, trainers, & locations.
     // I have the deleted methods if we need them in the future.
+
+
+    rc.graphData = function() {
+        var series = [];
+        var myCur = rc.curricula;
+        series.push([
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ]);
+
+
+        return series;
+    };
+
+
+    var categories = ['-55', '-50', '-45', '-40', '-35', '-30', '-25', '-20', '-15', '-10', '-05', '0'];
+    var test1 = {name: 'Inbound',data: [29.9, 71.5, 25.4, 43.2, 37.0, 33.0, 35.6, 48.5, 21.4, 19.1, 16.6, 54.4]};
+    var test2 = {name: 'Outbound',data: [19.3, 56.3, 23.1, 38.5, 32.9, 27.0, 30.6, 42.3, 17.4, 12.0, 9.1, 34.0]};
+
+
+    Highcharts.chart('container', {
+        options: {
+            chart: {
+                type: 'area'
+            }
+        },
+
+        title: {
+            text: 'Network Usage - Last 60 Minutes'
+        },
+        yAxis: {
+            title: {
+                text: 'Throughput MBit/s'
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'Minutes'
+            },
+            categories: categories
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: true
+            }
+        },
+        series: [
+            test1,
+            test2
+        ]
+    });
+
+
+
+
+
+
+});
+
+
+
+assignforce.directive('accordionDynamic', function(){
+    return{
+        restrict: 'ACE',
+        link: function(scope, element, attributes){
+            var ele = angular.element(element);
+            ele.bind('click',function(){
+                ele.toggleClass('active');
+                ele.next('.content').stop().slideToggle();
+                ele.parents('li').siblings().find('md-toolbar').removeClass('active');
+                ele.parents('li').siblings().find('.content').slideUp();
+                return false;
+            });
+        }
+    }
 });
