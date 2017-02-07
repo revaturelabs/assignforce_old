92ba81f5e75186f41259b488dcbb5fc03f9dc639
Nate Vardell <nvardell@gmail.com>
2017-02-07 12:08:06 -0500

    Work in progress

--- src/main/resources/public/js/controllers/reportController.js
+++ src/main/resources/public/js/controllers/reportController.js
@@ -10,17 +10,17 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
 
 
     $scope.options = [{
-        title: 'Item1',
-        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+        title: 'Graduates summary table for 2017',
+        content: 'Grad - Table'
     }, {
-        title: 'Item2',
-        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+        title: 'Graduates summary graph for 2017',
+        content: 'Grad - Graph'
     }, {
-        title: 'Item3',
-        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+        title: 'Incoming summary table for 2017',
+        content: 'Incoming Table'
     }, {
-        title: 'Item4',
-        content: 'Lorem Ipsum is simply dummy texts standard dummy text ever since the 1500s'
+        title: 'Incoming summary graph for 2017',
+        content: 'Incoming Graph'
     }];
 
 
@@ -134,14 +134,131 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
         }
     };
 
+
+
+    /*************************************************************/
+    /**
+     * This method will compute the required batch start date,
+     * 		given a required hire date.
+     *
+     * @param requiredDate
+     * @return nothing
+     */
+
+    rc.calcStarDate = function(requiredDate){
+
+        //Initializes a start date variable and assigns it the value in 'requiredDate'.
+        var sDate = ( requiredDate == undefined ) ? (new Date()) : requiredDate;
+
+
+        //Subtract 10 weeks from the 'requiredDate' to determine the 'startDate'.  **Using 11 week default.
+        sDate.setDate( sDate.getDate() - ( 7 * (rc.defWeeks)));
+
+        // This code segment allows for the batch start date to be pushed to the closest Monday.
+        switch(sDate.getDay()){
+
+            case 0 : sDate.setDate( sDate.getDate() + 1 );
+                break;
+
+            case 1 : sDate.setDate( sDate.getDate() );
+                break;
+
+            case 2 : sDate.setDate( sDate.getDate() - 1 );
+                break;
+
+            case 3 : sDate.setDate( sDate.getDate() - 2 );
+                break;
+
+            case 4 : sDate.setDate( sDate.getDate() - 3 );
+                break;
+
+            case 5 : sDate.setDate( sDate.getDate() - 4 );
+                break;
+
+            case 6 : sDate.setDate( sDate.getDate() -5 );
+                break;
+
+            default: break;
+        }
+
+
+        // var monthArr = [ 'JAN', 'FEB','MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC' ];
+        // var wkDayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+        //Formats the date to 'mm-dd-yyyy' and assigns the output for easier user visibility and comprehension.
+        var formattedDate = monthArr[sDate.getMonth()] + "-" + sDate.getDate() + "-" + sDate.getFullYear() + " (" + wkDayArr[sDate.getDay()] +")";
+
+        //Sets the 'startdate' equal to the formatted Date.
+        rc.startDate = formattedDate;
+
+    };
+
+    /*************************************************************/
+    /**
+     * This method will compute the number of batches needed to be made,
+     * 		given the number of required Trainee's.
+     *
+     * @param requiredTrainees
+     * @return neededBatches
+     */
+
+    rc.calcReqBatch = function(requiredTrainees){
+
+        //Compute the total number of Batches estimated.
+        var neededBatches = requiredTrainees/15;
+
+        /**
+         * 	Ensures the correct number of batches are created, regardless of any remainder of trainee's required.
+         * 		Example:  If the total number of required trainee's is 40 and the average batch
+         * 					size is 15, the resulting number of batches is '2.666666'.
+         * 					This result should be rounded up to accommodate for the remainder.
+         */
+        if (( neededBatches > Math.floor(neededBatches)) && (neededBatches < Math.ceil(neededBatches))){
+
+            neededBatches = Math.ceil( neededBatches );
+
+        }
+
+        //Sets the reportsController's 'requiredBatches' data value to the computed 'neededBatches' values.
+        rc.requiredBatches = neededBatches;
+
+        //Returns the reqBatches variable.  Needs to be both the
+        //'reqStartDate' and the 'reqBatches', at a later point.
+        return neededBatches;
+
+    };
+
+    /*************************************************************/
+
     // data
     rc.year = new Date().getFullYear();
+
+    //The number of graduates.  Used in
     rc.graduates = 15;
-    rc.futureGrads = 15;
+
+    //The date Trainee's are needed by.
+    rc.reqDate = new Date();
+
+    //Batch(s) StartDate variable.
+    rc.startDate = new Date();
+
+    //Default batch time-period
+    rc.defWeeks = 11;
+
+    //Number of Required Graduates
+    rc.requiredGrads = 40;
+
+    //The number of Batches needed to be created
+    rc.requiredBatches = 0;
 
     rc.currOrder = "name";
 
     rc.monthList = monthList;
+
+    var monthArr = [ 'JAN', 'FEB','MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC' ];
+    var wkDayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+    /*************************************************************/
 
     // page initialization
     // data gathering
@@ -170,66 +287,159 @@ assignforce.controller( "reportCtrl", function( $scope, batchService, curriculum
 
     rc.graphData = function() {
         var series = [];
-        var myCur = rc.curricula;
-        series.push([
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ]);
 
+        var curricula = rc.curricula;
+
+        angular.forEach(curricula, function (curr) {
+            var empty = {};
+            var data = [];
+            empty.name = curr.name;
+            rc.currSummary(curr).forEach(function (month) {
+                data.push(month);
+            });
+
+            empty.data = data;
+            // console.log(empty);
+            series.push(empty);
+            // console.log(series);
+        });
 
         return series;
     };
 
 
-    var categories = ['-55', '-50', '-45', '-40', '-35', '-30', '-25', '-20', '-15', '-10', '-05', '0'];
-    var test1 = {name: 'Inbound',data: [29.9, 71.5, 25.4, 43.2, 37.0, 33.0, 35.6, 48.5, 21.4, 19.1, 16.6, 54.4]};
-    var test2 = {name: 'Outbound',data: [19.3, 56.3, 23.1, 38.5, 32.9, 27.0, 30.6, 42.3, 17.4, 12.0, 9.1, 34.0]};
+
+    // var series = [];
+    // series = rc.graphData;
 
 
-    Highcharts.chart('container', {
-        options: {
-            chart: {
-                type: 'area'
-            }
+    // var fake = [0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 15, 0];
+    // var test = {name: '.NET', data: fake};
+    //
+    // var empty = {};
+    // empty.name = 'Java';
+    // empty.data = [0, 30, 0, 15, 0, 0, 0, 15, 0, 0, 15, 0];
+    //
+    // var series = [test, empty];
+
+    var categories = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    // var series = [{"name":[".NET"],"data":[0,0,0,0,0,0,0,15,0,0,15,0]},{"name":["Java"],"data":[15,45,0,15,15,0,15,0,15,0,0,30]},{"name":["SDET"],"data":[0,0,15,0,0,15,0,0,0,15,0,0]}];
+
+
+    // window.onload = console.log(rc.graphData);
+
+    // Highcharts.chart('container', {
+    //     chart: {
+    //         type: 'column'
+    //     },
+    //     title: {
+    //         text: 'Graduate Summary'
+    //     },
+    //     xAxis: {
+    //         categories: categories,
+    //         crosshair: true
+    //     },
+    //     yAxis: {
+    //         min: 0,
+    //         title: {
+    //             text: 'Graduates'
+    //         }
+    //     },
+    //     tooltip: {
+    //         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+    //         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+    //         '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+    //         footerFormat: '</table>',
+    //         shared: true,
+    //         useHTML: true
+    //     },
+    //     plotOptions: {
+    //         column: {
+    //             pointPadding: 0.2,
+    //             borderWidth: 0
+    //         }
+    //     },
+    //     series: rc.graphData()
+    // });
+
+
+    setTimeout(myGraph, 2000);
+
+
+    function myGraph() {Highcharts.chart('container', {
+        chart: {
+            type: 'column'
         },
-
         title: {
-            text: 'Network Usage - Last 60 Minutes'
-        },
-        yAxis: {
-            title: {
-                text: 'Throughput MBit/s'
-            }
+            text: 'Graduate Summary'
         },
         xAxis: {
-            title: {
-                text: 'Minutes'
-            },
-            categories: categories
+            categories: categories,
+            crosshair: true
         },
-        plotOptions: {
-            line: {
-                dataLabels: {
-                    enabled: true
-                },
-                enableMouseTracking: true
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Graduates'
             }
         },
-        series: [
-            test1,
-            test2
-        ]
-    });
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: rc.graphData()
+    })};
+
+
+    // var categories = ['-55', '-50', '-45', '-40', '-35', '-30', '-25', '-20', '-15', '-10', '-05', '0'];
+    // var test1 = {name: 'Inbound',data: [29.9, 71.5, 25.4, 43.2, 37.0, 33.0, 35.6, 48.5, 21.4, 19.1, 16.6, 54.4]};
+    // var test2 = {name: 'Outbound',data: [19.3, 56.3, 23.1, 38.5, 32.9, 27.0, 30.6, 42.3, 17.4, 12.0, 9.1, 34.0]};
+    //
+    //
+    // Highcharts.chart('container', {
+    //     options: {
+    //         chart: {
+    //             type: 'area'
+    //         }
+    //     },
+    //
+    //     title: {
+    //         text: 'Network Usage - Last 60 Minutes'
+    //     },
+    //     yAxis: {
+    //         title: {
+    //             text: 'Throughput MBit/s'
+    //         }
+    //     },
+    //     xAxis: {
+    //         title: {
+    //             text: 'Minutes'
+    //         },
+    //         categories: categories
+    //     },
+    //     plotOptions: {
+    //         line: {
+    //             dataLabels: {
+    //                 enabled: true
+    //             },
+    //             enableMouseTracking: true
+    //         }
+    //     },
+    //     series: [
+    //         test1,
+    //         test2
+    //     ]
+    // });
 
 
 
