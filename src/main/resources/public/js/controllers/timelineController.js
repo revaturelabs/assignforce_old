b33d2a6e8c75f096b3fd12034a4aa379c381de72
Dominic BRooks <dominiclbrooks@gmail.com>
2017-02-13 14:39:11 -0500

    Fixed and Polishings

--- src/main/resources/public/js/controllers/timelineController.js
+++ src/main/resources/public/js/controllers/timelineController.js
@@ -43,7 +43,9 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	tlc.maxTrainerNameCharacters = 6;
 	tlc.selectedCurriculum = 0;
 	tlc.trainersPerPage = 0;
+	tlc.realTrainersPerPage = 0;
 	tlc.trainerPage = 1;
+	tlc.realTrainerPage = 1;
 	tlc.maxTrainerPages = 1;
 	tlc.trainerListStartIndex = 0;
 	tlc.trainerListEndIndex = 0;
@@ -104,42 +106,29 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	var trainerNames;
 	
 	$scope.$on("repullTimeline", function(event, data){
-		tlc.getAllTrainers();
-		tlc.getAllBatches();
+		tlc.repull();
 	});
 
-	tlc.getAllBatches = function()
+	tlc.getAllBatches = new Promise(function(resolve, reject)
 	{
 	    batchService.getAll( function(response) {
 	        tlc.batches = response;
 	        tlc.getDateRange();
-
-	        if (!angular.isUndefined(tlc.trainers) && tlc.trainers !== null && !angular.isUndefined(tlc.trainerNames) && tlc.trainerNames !== null)
-	        {
-				tlc.projectTimeline(0);
-	        }
+	        resolve(1);
 	    }, function(error) {
+	    	resolve(0);
 	    });
-	}
+	});
 
-	tlc.getAllTrainers = function()
+	tlc.getAllTrainers = new Promise(function(resolve, reject)
 	{
 	    trainerService.getAll( function(response) {
 			tlc.trainers = response.map(function(trainer){return trainerColumnName(trainer)});
-			tlc.trainerNames = response.map(function(trainer){return (trainer.firstName + " " + trainer.lastName)});
-			tlc.getDateRange();
-
-	        if (!angular.isUndefined(tlc.batches) && tlc.batches !== null && !angular.isUndefined(tlc.trainerNames) && tlc.trainerNames !== null)
-	        {
-	        	tlc.projectTimeline(0);
-	        }
+			resolve(1);
 	    }, function(error) {
+	    	resolve(0);
 	    });
-	}
-	
-	tlc.getAllBatches();
-
-    tlc.getAllTrainers();
+	});
     
     curriculumService.getAll( function(response) {
         tlc.curricula = response;
@@ -172,6 +161,7 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	// Events for the timeline
 
 	$("#timeline").mousedown(function(evt){
+		evt.stopPropagation();
 
 		if(evt.offsetY > 30 && evt.offsetY < 1970){
 
@@ -197,6 +187,7 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 			$(".toastContainer").mousemove(function(evt){
 
 				evt.preventDefault();
+				evt.stopPropagation();
 
 				// Recalculate the scaling factor based on the number of milliseconds(more accuracy) currently on the timeline
 				tlc.scalingFactor = (new Date(tlc.maxDate).getTime() - new Date(tlc.minDate).getTime()) / 10;
@@ -224,34 +215,55 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 		}
 	});
 
-	$(".toastContainer").mouseup(function(){
+	$(".toastContainer").mouseup(function(evt){
 		// Erase the zoompoint(or move out of view)
 		tlc.projectTimeline(-100);
 		// Remove mousemove listener from the 
 		$(".toastContainer").off("mousemove");
+		evt.stopPropagation();
 	});
 	
-	$scope.$broadcast("repullTimeline");
-	
-    tlc.repull = function(){
-    	$scope.$broadcast("repullTimeline");
-    };
+    tlc.repullPromise = new Promise(function(resolve, reject){
+    	tlc.getAllBatches.then(function(result)
+    	{
+	    	tlc.getAllTrainers.then(function(result)
+	    	{
+	    		resolve(1);
+	    	});
+    	});
+    });
+    
+    tlc.repullPromise.then(function(result)
+    {
+    	if (result) { tlc.projectTimeline(0); }
+    }, function(error){});
+    
+    tlc.repull = function()
+    {
+        tlc.repullPromise.then(function(result)
+	    {
+        	if (result) { tlc.projectTimeline(0); }
+	    }, function(error){});
+    }
     
     //Pagination functions
 	tlc.changeTrainersPerPage = function()
 	{
 		var numTrainers = (tlc.trainers ? tlc.trainers.length : 0);
 		
-		if (tlc.trainersPerPage < 0 || !angular.isNumber(tlc.trainersPerPage)) {tlc.trainersPerPage = 0;}
+		tlc.realTrainersPerPage = tlc.trainersPerPage;
 		
-		tlc.trainersPerPage = Math.min(tlc.trainersPerPage, numTrainers);
+		if (tlc.realTrainersPerPage < 0 || !angular.isNumber(tlc.realTrainersPerPage)) {tlc.realTrainersPerPage = 0;}
+		
+		tlc.realTrainersPerPage = Math.min(tlc.realTrainersPerPage, numTrainers);
 		
 		tlc.trainerListStartIndex = 0;
 		
-		tlc.trainerListEndIndex = Math.min(tlc.trainersPerPage, numTrainers);
+		tlc.trainerListEndIndex = Math.min(tlc.realTrainersPerPage, numTrainers);
 		
-		tlc.trainerPage = 1;
-		tlc.maxTrainerPages = Math.ceil(numTrainers / tlc.trainersPerPage);
+		tlc.realTrainerPage = 1;
+		tlc.trainerPage = tlc.realTrainerPage;
+		tlc.maxTrainerPages = Math.ceil(numTrainers / tlc.realTrainersPerPage);
 		
 		tlc.previousPageButtonStatus();
 		tlc.nextPageButtonStatus();
@@ -259,10 +271,11 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	
 	tlc.previousTrainerPage = function()
 	{
-		tlc.trainerListStartIndex -= tlc.trainersPerPage;
-		tlc.trainerListEndIndex -= tlc.trainersPerPage;
+		tlc.trainerListStartIndex -= tlc.realTrainersPerPage;
+		tlc.trainerListEndIndex -= tlc.realTrainersPerPage;
 		
-		tlc.trainerPage -= 1;
+		tlc.realTrainerPage -= 1;
+		tlc.trainerPage = tlc.realTrainerPage;
 		
 		tlc.previousPageButtonStatus();
 		tlc.nextPageButtonStatus();
@@ -270,10 +283,55 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	
 	tlc.nextTrainerPage = function()
 	{
-		tlc.trainerListStartIndex += tlc.trainersPerPage;
-		tlc.trainerListEndIndex += tlc.trainersPerPage;
+		tlc.trainerListStartIndex += tlc.realTrainersPerPage;
+		tlc.trainerListEndIndex += tlc.realTrainersPerPage;
 		
-		tlc.trainerPage += 1;
+		tlc.realTrainerPage += 1;
+		tlc.trainerPage = tlc.realTrainerPage;
+		
+		tlc.previousPageButtonStatus();
+		tlc.nextPageButtonStatus();
+	}
+	
+	tlc.firstTrainerPage = function()
+	{
+		tlc.realTrainerPage = 1;
+		tlc.trainerPage = tlc.realTrainerPage;
+		
+		tlc.trainerListStartIndex = 0;
+		tlc.trainerListEndIndex = tlc.realTrainersPerPage;
+		
+		tlc.previousPageButtonStatus();
+		tlc.nextPageButtonStatus();
+	}
+	
+	tlc.lastTrainerPage = function()
+	{
+		tlc.realTrainerPage = tlc.maxTrainerPages;
+		tlc.trainerPage = tlc.realTrainerPage;
+		
+		tlc.trainerListStartIndex = tlc.realTrainersPerPage * (tlc.realTrainerPage - 1);
+		
+		var numTrainers = (tlc.trainers ? tlc.trainers.length : 0);
+		
+		tlc.trainerListEndIndex = tlc.trainerListStartIndex + tlc.realTrainersPerPage;
+		
+		tlc.previousPageButtonStatus();
+		tlc.nextPageButtonStatus();
+	}
+	
+	tlc.goToTrainerPage = function()
+	{
+		tlc.realTrainerPage = tlc.trainerPage;
+		
+		if (tlc.realTrainerPage < 0 || !angular.isNumber(tlc.realTrainerPage)) {tlc.realTrainerPage = 1;}
+		if (tlc.realTrainerPage > tlc.maxTrainerPages) { tlc.realTrainerPage = tlc.maxTrainerPages; }
+		
+		tlc.trainerListStartIndex = tlc.realTrainersPerPage * (tlc.realTrainerPage - 1);
+		
+		var numTrainers = (tlc.trainers ? tlc.trainers.length : 0);
+		
+		tlc.trainerListEndIndex = tlc.trainerListStartIndex + tlc.realTrainersPerPage;
 		
 		tlc.previousPageButtonStatus();
 		tlc.nextPageButtonStatus();
@@ -282,7 +340,7 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	tlc.previousPageButtonStatus = function()
 	{
 		//True = disabled, false = enabled.
-		if (tlc.trainerListStartIndex == 0 || tlc.trainersPerPage == 0) { tlc.previousPageButtonDisabled = true; }
+		if (tlc.trainerListStartIndex == 0 || tlc.realTrainersPerPage == 0) { tlc.previousPageButtonDisabled = true; }
 		else { tlc.previousPageButtonDisabled = false; }
 	}
 	
@@ -291,7 +349,7 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 		var numTrainers = (tlc.trainers ? tlc.trainers.length : 0);
 		
 		//True = disabled, false = enabled.
-		if (tlc.trainerListStartIndex + tlc.trainersPerPage >= numTrainers || tlc.trainersPerPage == 0) { tlc.nextPageButtonDisabled = true; }
+		if (tlc.trainerListStartIndex + tlc.realTrainersPerPage >= numTrainers || tlc.realTrainersPerPage == 0) { tlc.nextPageButtonDisabled = true; }
 		else { tlc.nextPageButtonDisabled = false; }
 	}
 	
@@ -299,11 +357,26 @@ app.controller("TimelineCtrl", function($scope, $window, batchService, calendarS
 	
 	tlc.projectTimeline = function(yOffset)
 	{
-		tlc.filteredTrainers = tlc.trainers.filter(tlc.removeTrainersOutOfPage);
-		
-		tlc.filteredBatches = tlc.batches.filter(tlc.removeNoTrainer).filter(tlc.removeIrrelevantBatches);
-		
-		projectTimeline($window.innerWidth, tlc.minDate, tlc.maxDate, yOffset, tlc.filteredBatches, $scope.$parent, calendarService.countWeeks, tlc.filteredTrainers, tlc.maxTrainerNameCharacters, tlc.selectedCurriculum);
+		if (tlc.trainers && tlc.batches)
+		{
+			tlc.filteredTrainers = tlc.trainers.filter(tlc.removeTrainersOutOfPage);
+			
+			tlc.filteredBatches = tlc.batches.filter(tlc.removeNoTrainer).filter(tlc.removeIrrelevantBatches);
+			
+			tlc.filteredTrainers.sort(function(a,b){
+				if(a.trainerID < b.trainerID){
+					return -1;
+				}
+				else if(a.trainerID > b.trainerID){
+					return 1;
+				}
+				else{
+					return 0;
+				}
+			});
+			
+			projectTimeline($window.innerWidth, tlc.minDate, tlc.maxDate, yOffset, tlc.filteredBatches, $scope.$parent, calendarService.countWeeks, tlc.filteredTrainers, tlc.selectedCurriculum);
+		}
 	}
 });
 
@@ -314,12 +387,12 @@ function trainerColumnName(trainer)
 }
 
 // Draw timeline
-function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, parentScope, numWeeks, trainerNames, maxTrainerNameCharacters, selectedCurriculum){
+function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, parentScope, numWeeks, trainerNames, selectedCurriculum){
 	//Timeline variables
 	var margin = {top: 80, right: 16, bottom: 32, left:72},
 	width = windowWidth - margin.left - margin.right,
 	height = 2000 - margin.top - margin.bottom,
-	xPadding = 8 * maxTrainerNameCharacters;
+	xPadding = 72;
 	
 	//Define Scales
 	var colorScale = d3.scale.category20();
@@ -429,7 +502,7 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 		  
 		  msg += d.curriculum ? ("<span style='color:orange'>" + d.curriculum.name + "</span> Batch <br/>") : "<span style='color:red'>No curriculum</span> for this batch. <br/>";
 		  msg += d.trainer ? ("Trainer:  <span style='color:gold'>" + d.trainer.firstName + " " + d.trainer.lastName + "</span> <br/>") : "<span style='color:gold'>No trainer</span> for this batch. <br/>";
-		  msg += d.cotrainer ? ("Cotrainer:  <span style='color:gold'>" + d.cotrainer.firstName + " " + d.trainer.lastName + "</span> <br/>") : "<span style='color:gold'>No cotrainer</span> for this batch. <br/>";
+		  msg += d.cotrainer ? ("Cotrainer:  <span style='color:gold'>" + d.cotrainer.firstName + " " + d.cotrainer.lastName + "</span> <br/>") : "<span style='color:gold'>No cotrainer</span> for this batch. <br/>";
 		  
 		  return msg;
 	  });
@@ -543,8 +616,8 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 				}
 				return y;
 			})
-			.attr('x', function(d) {return xScale(d.trainer ? trainerColumnName(d.trainer) : 'No trainer')-15;})
-			.attr('width', 30)
+			.attr('width', 32)
+			.attr('x', function(d) {return xScale(d.trainer ? trainerColumnName(d.trainer) : 'No trainer') - (d3.select(this).attr("width") / 2);})
 			.attr('height', function(d) {
 				var start = yScale(new Date(d.startDate));
 				var end = yScale(new Date(d.endDate));
@@ -561,7 +634,7 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 			    var mouse_coordinates;
 			    
 			    mouse_coordinates = d3.mouse(this);
-				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y") - 8, mouse_coordinates[0] - d3.select(this).attr("x") - 16]).show(d);
+				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y") - 8, mouse_coordinates[0] - d3.select(this).attr("x") - (d3.select(this).attr("width") / 2)]).show(d);
 				d3.event.stopPropagation();
 			})
 			.on('mouseout', function(d)
@@ -569,8 +642,20 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 				tip.hide(d);
 				d3.event.stopPropagation();
 			})
-			.on('mousedown', function()
+			.on('click', function(d){
+				tip.hide(d);
+				parentScope.bCtrl.highlightBatch(d);
+				parentScope.$apply();
+				d3.event.stopPropagation();
+			})
+			.on('mousedown', function(d)
 			{
+				tip.hide(d);
+				d3.event.stopPropagation();
+			})
+			.on('mouseup', function(d)
+			{
+			    tip.show(d);
 				d3.event.stopPropagation();
 			})
 			.on('mousemove', function(d)
@@ -579,12 +664,7 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 			    
 			    mouse_coordinates = d3.mouse(this);
 			    tip.hide(d);
-				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y") - 8, mouse_coordinates[0] - d3.select(this).attr("x") - 16]).show(d);
-				d3.event.stopPropagation();
-			})
-			.on('click', function(d){
-				parentScope.bCtrl.highlightBatch(d);
-				parentScope.$apply();
+				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y") - 8, mouse_coordinates[0] - d3.select(this).attr("x") - (d3.select(this).attr("width") / 2)]).show(d);
 				d3.event.stopPropagation();
 			})
 			.style('fill', function(d) {return colorScale(d.curriculum ? d.curriculum.name : 'No curriculum');});
@@ -598,6 +678,44 @@ function projectTimeline(windowWidth, minDate, maxDate, yCoord, timelineData, pa
 				return (y+25);
 			})
 			.attr('x', function(d) {return xScale(d.trainer ?  trainerColumnName(d.trainer) : 'No trainer')-7;})
+			.on('mouseover', function(d)
+			{
+			    var mouse_coordinates;
+			    
+			    mouse_coordinates = d3.mouse(this);
+				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y"), mouse_coordinates[0] - d3.select(this).attr("x") - 8]).show(d);
+				d3.event.stopPropagation();
+			})
+			.on('mouseout', function(d)
+			{
+				tip.hide(d);
+				d3.event.stopPropagation();
+			})
+			.on('click', function(d){
+				tip.hide(d);
+				parentScope.bCtrl.highlightBatch(d);
+				parentScope.$apply();
+				d3.event.stopPropagation();
+			})
+			.on('mousedown', function(d)
+			{
+				tip.hide(d);
+				d3.event.stopPropagation();
+			})
+			.on('mouseup', function(d)
+			{
+			    tip.show(d);
+				d3.event.stopPropagation();
+			})
+			.on('mousemove', function(d)
+			{
+			    var mouse_coordinates;
+			    
+			    mouse_coordinates = d3.mouse(this);
+			    tip.hide(d);
+				tip.offset([mouse_coordinates[1] - d3.select(this).attr("y"), mouse_coordinates[0] - d3.select(this).attr("x") - 8]).show(d);
+				d3.event.stopPropagation();
+			})
 			.text(function(d) {return numWeeks(d.startDate,d.endDate) + " W E E K S";})
 				.attr("dy", 0);
 	
