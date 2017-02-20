a94edc101c13de13a72025d358fd61a2ba050b89
Nate Vardell <nvardell@gmail.com>
2017-02-20 10:02:53 -0500

    Fixing merge conflicts

--- src/main/resources/public/js/controllers/dialogs/skillDialogController.js
+++ src/main/resources/public/js/controllers/dialogs/skillDialogController.js
@@ -4,11 +4,12 @@
 
 var assignforce = angular.module("batchApp");
 
-assignforce.controller("skillDialogCtrl", function ($scope, $mdDialog, skillService, trainerService) {
+assignforce.controller("skillDialogCtrl", function ($scope, $mdDialog, $mdToast, skillService) {
 
     var sdc = this;
 
     //functions
+
     //close dialog
     sdc.cancel = function () {
         $mdDialog.cancel();
@@ -16,7 +17,7 @@ assignforce.controller("skillDialogCtrl", function ($scope, $mdDialog, skillServ
 
     // adds/removes skill from selectedSkills
     sdc.toggle = function(obj) {
-        sdc.selectedSkills = sdc.trainer.skill;
+        sdc.selectedSkills = sdc.trainer.skills;
 
         var idx = sdc.selectedSkills.indexOf(obj);
         if (idx == -1) {
@@ -28,7 +29,6 @@ assignforce.controller("skillDialogCtrl", function ($scope, $mdDialog, skillServ
 
     // checks box if location/room is in selectedList
     sdc.exists = function(obj) {
-        // selectedSkills equals trainers skill
         return sdc.selectedSkills.indexOf(obj) > -1;
     };
 
@@ -43,13 +43,6 @@ assignforce.controller("skillDialogCtrl", function ($scope, $mdDialog, skillServ
                     $mdDialog.cancel();
                 });
             }
-            //end
-            sdc.trainer.skill = sdc.selectedSkills;
-            trainerService.update(sdc.trainer, function(){
-                //rePullTrainer function will be called
-            }, function (){
-                sdc.showToast("Error updating trainer.");
-            });
             $mdDialog.hide();
         }
     };
