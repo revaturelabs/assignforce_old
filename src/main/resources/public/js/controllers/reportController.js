c677ded41ef70400a675ea67e87a9fe9eb8d4966
Nate Vardell <nvardell@gmail.com>
2017-02-20 16:42:01 -0500

    Re-added my deleted function for creating graph.

--- src/main/resources/public/js/controllers/reportController.js
+++ src/main/resources/public/js/controllers/reportController.js
@@ -586,6 +586,42 @@ assignforce.controller( "reportCtrl", function( $scope, $mdPanel, settingService
 
     // var chart = new Highcharts.Chart(chartOptions);
 
+    $scope.myGraph = function() {
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Graduate Summary'
+            },
+            xAxis: {
+                categories: monthList,
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Graduates'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: rc.graphData()
+        })};
+
+
     rc.chartOptions = {
         chart: {
             type: 'column'
