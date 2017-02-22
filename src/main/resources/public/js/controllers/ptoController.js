39b6ae644ce6ca48e94db1cc5b870edf8025caec
Nate Vardell <nvardell@gmail.com>
2017-02-22 03:50:09 -0500

    Altered to try and allow myself to add events to Tylers calendar

--- src/main/resources/public/js/controllers/ptoController.js
+++ src/main/resources/public/js/controllers/ptoController.js
@@ -1,17 +1,30 @@
-
 var assignforce = angular.module("batchApp");
 
 assignforce.controller("ptoCtrl", function ($scope, $mdDialog, ptoService) {
 
     var ptoc = this;
 
+    try{
+        ptoService.authorize();
+    } catch(error){
+
+    }
+
     ptoc.cancel = function () {
         $mdDialog.cancel();
+
+        $mdDialog.show({
+            templateUrl: "html/templates/calendarTemplate.html",
+            controller: "trainerCtrl",
+            controllerAs: "tCtrl",
+            bindToController: true,
+            clickOutsideToClose: true
+        })
     };
 
     ptoc.send = function (isValid) {
         if(isValid){
-            ptoService.sendRequest();
+            ptoService.addPto(ptoc.trainer, ptoc.startDate, ptoc.endDate);
         }
     };
 });
