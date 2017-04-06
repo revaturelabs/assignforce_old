4a05ca8ce7ce60d103bdcc241a89c297550ab56f
August Duet <august@revature.com>
2017-04-06 01:24:12 -0400

    adding data access for new settings rules

---
+++ src/main/java/com/revature/assignforce/service/BatchNamePatternDaoService.java
@@ -0,0 +1,9 @@
+package com.revature.assignforce.service;
+
+import com.revature.assignforce.domain.BatchNamePattern;
+
+/**
+ * Created by August Duet on 4/6/2017.
+ */
+public class BatchNamePatternDaoService extends DaoService<BatchNamePattern, Integer>{
+}
