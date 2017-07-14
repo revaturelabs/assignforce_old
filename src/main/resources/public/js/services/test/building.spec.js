7af463ff69877a8e559e49b2826172f11fa74d4b
Roger E. Newman <rogerenewmanii@gmail.com>
2017-07-14 16:06:25 -0400

    Shortened service pathes and test pathes; changed karma.conf.js

--- src/main/resources/public/js/services/test/building.spec.js
+++ src/main/resources/public/js/services/test/building.spec.js
@@ -5,15 +5,31 @@ describe('Building factory', function(){
 
     var Building;
 
-    beforeEach(angular.mock.module('api.building'));
+    var EmptyBuilding;
 
+    // var BuildingTest = $resource('/api/v2/building/:id',{id: '@id'},{update:{method:'PUT', url:'api/v2/building'}});
+
+    beforeEach(angular.mock.module('batchApp'));
 
     beforeEach(inject(function(_Building_) {
-        Building = _Building_;
+        Building = _Building_
     }));
 
-
+    //A simple test to verify the Building service exist
     it('should exist', function(){
         expect(Building).toBeDefined();
+    });
+
+    //A set of tests for our Building.getEmptyBuilding() method
+    describe('.getEmptyBuilding()', function(){
+
+        //A test to verify the method getEmptyBuilding exists
+        it('should exist', function(){
+            expect(Building.getEmptyBuilding()).toBeDefined();
+        });
+
+        it('give empty building object', function(){
+            expect(Building.getEmptyBuilding()).toEqual(EmptyBuilding);
+        })
     });
 });
