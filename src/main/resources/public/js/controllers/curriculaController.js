8fb144d574eeca89e07a110e489d6dc963e1d389
katt3985 <Kathrine.Elain@gmail.com>
2017-10-02 13:37:15 -0400

    Refactored curriculaController

--- src/main/resources/public/js/controllers/curriculaController.js
+++ src/main/resources/public/js/controllers/curriculaController.js
@@ -64,32 +64,28 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, curriculum
     };
 
     //focus functions
-    //create a focus
-    //I want to fix this to be readable - Sam
-    cc.createFocus = function (focusForm) {
+    cc.createCurriculum = function (focusForm,isCore) {
+
         //show a hidden field with a list of skill to select from, a name field, and a save button
         if(focusForm.$valid){
-            var skillList = [];
-            for(var i = 0; i < cc.selectedSkills.length; i++){
-                for(var j = 0; j < cc.skills.length; j++){
-                    if(cc.skills[j].skillId == cc.selectedSkills[i]){
-                        skillList.push(cc.skills[j]);
-                        break;
-                    }
-                }
-            }
 
-            var curriculum = {
-                name    : cc.focusName,
+            let selectedSkills = cc.selectedSkills.map((x) => {
+                return parseInt(x);
+            });
+            let skillList = cc.skills
+                .filter( (skill) => selectedSkills.includes(skill.skillId) );
+
+            let curriculum = {
+                name    : isCore?cc.coreName:cc.focusName,
                 skills  : skillList,
                 active  : true,
-                core    : false
+                core    : isCore
             };
 
             curriculumService.create(curriculum, function () {
-                cc.showToast("Focus created")
+                cc.showToast("" + isCore? "Core":"Focus" +" created")
             }, function () {
-                cc.showToast("Failed to create focus")
+                cc.showToast("Failed to create " + isCore? "core":"focus")
             })
 
             //reload curriculum
@@ -101,44 +97,20 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, curriculum
 
         cc.selectedSkills = [];
         cc.focusName = undefined;
-    };
 
+    };
+    //create a focus
+    //I want to fix this to be readable - Sam
+    cc.createFocus = function (focusForm) {
+
+        cc.createCurriculum(focusForm,false);
+        cc.focusName = undefined;
+
+    };
 
     //create a core
     cc.createCore = function (coreForm) {
-        //show a hidden field with a list of skill to select from, a name field, and a save button
-        if(coreForm.$valid){
-            var skillList = [];
-            for(var i = 0; i < cc.selectedSkills.length; i++){
-                for(var j = 0; j < cc.skills.length; j++){
-                    if(cc.skills[j].skillId == cc.selectedSkills[i]){
-                        skillList.push(cc.skills[j]);
-                        break;
-                    }
-                }
-            }
-
-            var curric = {
-                name    : cc.coreName,
-                skills  : skillList,
-                active  : true,
-                core    : true
-            };
-
-            curriculumService.create(curric, function () {
-                cc.showToast("Core created")
-            }, function () {
-                cc.showToast("Failed to create core")
-            })
-
-            //reload curric
-            cc.curricula.push(curric);
-
-        } else {
-            cc.showToast("Missing input fields.")
-        }
-
-        cc.selectedSkills = [];
+        cc.createCurriculum(coreForm,true);
         cc.coreName = undefined;
     };
 
