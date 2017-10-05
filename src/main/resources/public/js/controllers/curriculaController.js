be80ab539f09f0b53e50aca6d98f657af3d4fef0
OlerChris <olerchris@gmail.com>
2017-10-05 10:27:11 -0400

    multiselect

--- src/main/resources/public/js/controllers/curriculaController.js
+++ src/main/resources/public/js/controllers/curriculaController.js
@@ -7,8 +7,7 @@ var assignforce = angular.module( "batchApp" );
 assignforce.controller("curriculaCtrl", function ($scope, $rootScope, $mdDialog, curriculumService, skillService) {
     var cc = this;
 
-    $scope.isManager = $rootScope.role == "VP of Technology";
-    // $scope.skillToggle = false;
+    $scope.isManager = $rootScope.role === "VP of Technology";
 
     //functions
 
@@ -64,32 +63,28 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, $mdDialog,
     };
 
     //focus functions
-    //create a focus
-    //I want to fix this to be readable - Sam
-    cc.createFocus = function (focusForm) {
+    cc.createCurriculum = function (focusForm,isCore) {
+
         //show a hidden field with a list of skill to select from, a name field, and a save button
         if(focusForm.$valid){
-            var skillList = [];
-            for(var i = 0; i < cc.selectedSkills.length; i++){
-                for(var j = 0; j < cc.skills.length; j++){
-                    if(cc.skills[j].skillId == cc.selectedSkills[i]){
-                        skillList.push(cc.skills[j]);
-                        break;
-                    }
-                }
-            }
 
-            var curriculum = {
-                name    : cc.focusName,
+            let selectedSkills = cc.selectedSkills.map((x) => {
+                return parseInt(x);
+            });
+            let skillList = cc.skills
+                .filter( (skill) => selectedSkills.includes(skill.skillId) );
+
+            let curriculum = {
+                name    : isCore?cc.coreName:cc.focusName,
                 skills  : skillList,
                 active  : true,
-                core    : false
+                core    : isCore
             };
 
             curriculumService.create(curriculum, function () {
-                cc.showToast("Focus created")
+                cc.showToast("" + isCore? "Core":"Focus" +" created")
             }, function () {
-                cc.showToast("Failed to create focus")
+                cc.showToast("Failed to create " + isCore? "core":"focus")
             })
 
             //reload curriculum
@@ -101,44 +96,20 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, $mdDialog,
 
         cc.selectedSkills = [];
         cc.focusName = undefined;
-    };
 
+    };
+    //create a focus
+    //I want to fix this to be readable - Sam
+    cc.createFocus = function (focusForm) {
+
+        cc.createCurriculum(focusForm,false);
+        cc.focusName = undefined;
+
+    };
 
     //create a core
     cc.createCore = function (coreForm) {
-        //show a hidden field with a list of skill to select from, a name field, and a save button
-        if(coreForm.$valid){
-            var skillList = [];
-            for(var i = 0; i < cc.selectedSkills.length; i++){
-                for(var j = 0; j < cc.skills.length; j++){
-                    if(cc.skills[j].skillId == cc.selectedSkills[i]){
-                        skillList.push(cc.skills[j]);
-                        break;
-                    }
-                }
-            }
-
-            var curric = {
-                name    : cc.coreName,
-                skills  : skillList,
-                active  : true,
-                core    : true
-            };
-
-            curriculumService.create(curric, function () {
-                cc.showToast("Core created")
-            }, function () {
-                cc.showToast("Failed to create core")
-            })
-
-            //reload curric
-            cc.curricula.push(curric);
-
-        } else {
-            cc.showToast("Missing input fields.")
-        }
-
-        cc.selectedSkills = [];
+        cc.createCurriculum(coreForm,true);
         cc.coreName = undefined;
     };
 
@@ -207,35 +178,67 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, $mdDialog,
     //Grabs all Skills
     skillService.getAll(function (response) {
         cc.skills = response;
+        $rootScope.skills = cc.skills;
     }, function () {
         cc.showToast("Could not fetch skills.")
     });
 
     //Show Edit Curriculum Dialog
     $scope.showCurriculum = function(event) {
-        var prompt = $mdDialog.prompt()
-          .title('New Curriculum')
-          .placeholder('curriculumName')
-          .ariaLabel('curriculumName')
-          .initialValue('Curriculum Name')
-          .ok('Create')
-          .cancel('Cancel')
-          .targetEvent(event);
-       $mdDialog.show(prompt).then(function(result){
-            var curric = {
-                name    : result,
-                skills  : {},
-                active  : true,
-                core    : true
-            };
-            curriculumService.create(curric, function () {
-                cc.showToast("Core created")
-            }, function () {
-                cc.showToast("You're not authorized Scrub")
-            })
-
-            cc.curricula.push(curric);
+       $mdDialog.show({
+            targetEvent: event,
+            template:
+            '<md-dialog>' +
+            '   <md-dialog-content>' +
+            '       <form name = curriculumForm>' +
+            '           <h3>Core Curriculum</h3>' +
+            '	        <md-input-container>' +
+            '		        <label>Curriculum Name</label>' +
+            '		        <input name="curriculumName" ng-model="coreN" ng-init= "New Curriculum">' +
+            '           </md-input-container>' +
+            '           <br>' +
+            '	        <md-input-container>' +
+            '		        <h5>Skill Selection</h5>' +
+            '               <select ng-model="skillz" multiple="multiple">' +
+            '                   <option ng-repeat="x in skills">{{x.name}}</option>' +
+            '               </select>' +
+            '           </md-input-container>' +
+            '       </form>' +
+            '   </md-dialog-content>' +
+            '   <md-dialog-actions>' +
+            '       <md-button ng-click="cancel()" class="md-primary">Cancel</md-button>' +
+            '       <md-button ng-click="createCore()" class="md-primary">Save</md-button>' +
+            '   </md-dialog-actions>' +
+            '</md-dialog>',
+            locals: {
+                       skills: $rootScope.skills
+                     },
+            controller: DialogController
        });
+       function DialogController($scope, $mdDialog, skills) {
+
+           $scope.skills = skills;
+
+           $scope.cancel = function() {
+            $mdDialog.cancel();
+           }
+           $scope.createCore = function() {
+                var curric = {
+                           name    : $scope.coreN,
+                           skills  : $scope.skillz,
+                           active  : true,
+                           core    : true
+                       };
+                       curriculumService.create(curric, function () {
+                           cc.showToast("Core created")
+                       }, function () {
+                           cc.showToast("You're not authorized Scrub")
+                       })
+
+                       cc.curricula.push(curric);
+                $mdDialog.hide();
+           }
+       }
     };
 
     //Show Edit Focus Dialog
@@ -265,8 +268,6 @@ assignforce.controller("curriculaCtrl", function ($scope, $rootScope, $mdDialog,
        });
     };
     //variables
-    // cc.curricula;
-    // cc.skills;
     cc.selectedSkills = [];
     cc.focusName = undefined;
     cc.coreName = undefined; //added for add Core usecase
