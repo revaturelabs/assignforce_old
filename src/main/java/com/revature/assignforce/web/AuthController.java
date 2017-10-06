bbca83b49882ab5a96ef769cff0871de29064f05
Phillip Willis <wilbur6689@gmail.com>
2017-10-06 10:08:12 -0400

    added some swagger annotations to the first three controllers

--- src/main/java/com/revature/assignforce/web/AuthController.java
+++ src/main/java/com/revature/assignforce/web/AuthController.java
@@ -2,6 +2,11 @@ package com.revature.assignforce.web;
 
 import com.revature.assignforce.domain.Force;
 import com.revature.assignforce.domain.Employee;
+import com.revature.assignforce.service.ActivatableObjectDaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -18,7 +23,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-
+@RequestMapping(value = "/api/v2")
+@Api(value = "Authorization Controller", tags = "Auhtorization Controller")
 public class AuthController {
 
     @Autowired
@@ -31,7 +37,14 @@ public class AuthController {
     @Autowired
     private OAuth2ClientContext context;
 
-    @RequestMapping(value= "auth/userinfo", produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @ApiOperation(value = "Gets information of Employee", response= ActivatableObjectDaoService.class)
+    @ApiResponses({
+            @ApiResponse(code=200, message ="Successfully received employee information"),
+            @ApiResponse(code=400, message ="Bad Request, the information recieved maybe invalid"),
+            @ApiResponse(code=500, message ="Cannot retrieve Employee information")
+    })
+    @RequestMapping(value= "/userinfo", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Employee> getInfo(OAuth2Authentication auth)
     {
         Employee emp = force.getCurrentEmployee(auth);
