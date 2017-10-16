35676b1dc5cb7a6733dce5b6e59804afec01dc38
tjp824 <tjp824@gmail.com>
2017-10-16 09:30:01 -0400

    fix case

--- src/main/java/com/revature/assignforce/config/SwaggerConfig.java
+++ src/main/java/com/revature/assignforce/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.revature.assignforce.Config;
+package com.revature.assignforce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
